9f14bb734cc18453828a5a95b9b711de25b49fe6
Erich Richter <erich.richter@gmail.com>
2020-01-30 01:59:19 -0800

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,15 @@ var arr1 = ['1','2','3','4','5','6','7','8','9']
 
 var spot1 =[]
 
+var xCheck123 = function(){
+  if($("#1:contains('X')").length && $("#2:contains('X')").length && $("#3:contains('X')").length) {
+alert("X wins")
+}
 
+}
 for (i = 0 ; i < arr1.length ; i ++){
   console.log(arr1[i])
-
+ 
 }
 
   
@@ -20,11 +25,36 @@ $('#1').on('click', function (event) {
   if($("#1:contains('X')").length) {
   
       alert("this spot is X")
-  }
+  xCheck123();
+    }
   
+  });
+
+
+  $("#2").html(arr1[i,1]);
+  $('#2').on('click', function (event) {
+    $("#2").text("X");
   
+    if($("#2:contains('X')").length) {
+    
+        alert("this spot is X")
+        xCheck123();
+      }
+    
+    });
+
+    $("#3").html(arr1[i,2]);
+  $('#3').on('click', function (event) {
+    $("#3").text("X");
   
-  
-});
+    if($("#3:contains('X')").length) {
+    
+        alert("this spot is X")
+        xCheck123();
+      }
+    
+    });
+
+
 
 });
